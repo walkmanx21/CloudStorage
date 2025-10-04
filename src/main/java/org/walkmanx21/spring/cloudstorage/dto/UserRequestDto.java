b0adcde935f9c3839424f7888c113529f9745258
walkmanx21 <walkmanx21@gmail.com>
2025-10-04 14:37:02 +0300

    правка опечаток в информационных сообщениях UserRequestDto

--- src/main/java/org/walkmanx21/spring/cloudstorage/dto/UserRequestDto.java
+++ src/main/java/org/walkmanx21/spring/cloudstorage/dto/UserRequestDto.java
@@ -23,8 +23,8 @@ public class UserRequestDto {
     private String username;
 
     @NotBlank(message = "Поле password не должно быть пустым, состоять из пробелов")
-    @Size(min = 5, max = 20, message = "Поле username должно быть размером от 5 до 20 символов")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Поле username должно состоять из латинских букв и цифр")
+    @Size(min = 5, max = 20, message = "Поле password должно быть размером от 5 до 20 символов")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Поле password должно состоять из латинских букв и цифр")
     @Schema(description = "DTO пользователя", example = "yudayidydu22123")
     private String password;
 }
